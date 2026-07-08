991e75e0c751038b5ab3c7da5b4274da42daae57
qwer123 <jysun0514@gmil.com>
2026-07-08 10:15:19 +0900

    fix: OpenRouter 설정 수정

--- server/llm.py
+++ server/llm.py
@@ -4,6 +4,6 @@ from server.config import OPENAI_API_KEY
 llm=ChatOpenAI(
     model="gpt-4.1-mini",
     temperature=0,
-    api_key=OPENAI_API_KEY
-    base_url="https://openrouter.ai/api/v1,
+    api_key=OPENAI_API_KEY,
+    base_url="https://openrouter.ai/api/v1",
 )

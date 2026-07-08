37fa8d3807092bcee902fd41aa60f5854bbf6f1c
qwer123 <jysun0514@gmil.com>
2026-07-08 10:41:31 +0900

    fix: LLM 및 prompt 설정 수정

--- server/llm.py
+++ server/llm.py
@@ -2,7 +2,7 @@ from langchain_openai import ChatOpenAI
 from server.config import OPENAI_API_KEY
 
 llm=ChatOpenAI(
-    model="gpt-4.1-mini",
+    model="openai/gpt-4.1-mini",
     temperature=0,
     api_key=OPENAI_API_KEY
 )
